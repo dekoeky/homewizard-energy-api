4d44f70865d81cb651f4486677b5753829b35b57
Wesley Van Koeckhoven <wvk@outlook.be>
2023-10-19 18:33:26 +0200

    Discovery Service

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,14 +1,21 @@
-using WebApplication1.Controllers;
+﻿using System.Text;
+using Serilog;
+using WebApplication1.Services;
+
+Console.OutputEncoding = Encoding.UTF8; //Emoji support :)
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(new LoggerConfiguration().ReadFrom.Configuration(builder.Configuration).CreateLogger());
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddHostedService<DiscoveryService>();
+builder.Services.AddHostedService<HomewizardDiscoveryService>();
 
 var app = builder.Build();
 
--- WebApplication1/Services/DiscoveryService.cs
+++ WebApplication1/Services/DiscoveryService.cs
@@ -1,45 +1,44 @@
 ﻿using System.Collections.Concurrent;
 using Makaretu.Dns;
+using Timer = System.Timers.Timer;
 
-namespace WebApplication1.Controllers;
+namespace WebApplication1.Services;
 
-public class DiscoveryService : BackgroundService
+public class HomewizardDiscoveryService : IHostedService, IDisposable
 {
     private const string DomainPartOne = "_hwenergy";
     private const string DomainPartTwo = "_tcp";
-    private static readonly DomainName Service = new(DomainPartOne, DomainPartTwo, "_local");
-    private readonly TimeSpan delay = TimeSpan.FromMinutes(1);
-    private readonly ILogger<DiscoveryService> _logger;
 
+    private static readonly TimeSpan Delay = TimeSpan.FromSeconds(60);
+    private readonly ILogger<HomewizardDiscoveryService> _logger;
+    private readonly Timer _timer = new(Delay);
     private readonly ServiceDiscovery _discovery = new();
-    private ConcurrentBag<string> _discovered = new();
+    private static readonly DomainName Service = new(DomainPartOne, DomainPartTwo);
+    private readonly ConcurrentBag<string> _discovered = new();
 
-    public DiscoveryService(ILogger<DiscoveryService> logger)
+    public HomewizardDiscoveryService(ILogger<HomewizardDiscoveryService> logger)
     {
         _logger = logger;
     }
 
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    public Task StartAsync(CancellationToken cancellationToken)
     {
         _discovery.ServiceInstanceDiscovered += _discovery_ServiceInstanceDiscovered;
+        _timer.Elapsed += Timer_Elapsed;
 
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            _discovery.QueryServiceInstances(Service);
-            await Task.Delay(delay, stoppingToken);
-        }
+        _timer.Start();
 
-        _discovery.ServiceInstanceDiscovered -= _discovery_ServiceInstanceDiscovered;
+        return Task.CompletedTask;
     }
 
     private void _discovery_ServiceInstanceDiscovered(object? sender, ServiceInstanceDiscoveryEventArgs e)
     {
+        _logger.LogDebug("Service Instance Discovered: {ServiceInstanceName}", e.ServiceInstanceName);
         var parent = e.ServiceInstanceName.Parent();
 
         if (DomainPartOne.Equals(parent.Labels.ElementAtOrDefault(0), StringComparison.InvariantCultureIgnoreCase))
             Discovered(e.ServiceInstanceName.Labels.First());
     }
-
     private void Discovered(string hostname)
     {
         lock (_discovered)
@@ -48,6 +47,25 @@ public class DiscoveryService : BackgroundService
 
             _discovered.Add(hostname);
         }
-        _logger.LogInformation("Discovered {hostname}", hostname);
+        _logger.LogInformation("⚡ Discovered {hostname}", hostname);
+    }
+
+    private void Timer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e) => _discovery.QueryServiceInstances(Service);
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _timer.Stop();
+
+        _timer.Elapsed -= Timer_Elapsed;
+        _discovery.ServiceInstanceDiscovered -= _discovery_ServiceInstanceDiscovered;
+
+        return Task.CompletedTask;
+    }
+
+    public void Dispose()
+    {
+        _timer.Stop();
+        _timer.Dispose();
+        _discovery.Dispose();
     }
 }
